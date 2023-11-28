f906b8655df76af6c2e0a4bd86dfba92fd4ca183
meeramenon07 <46912602+meeramenon07@users.noreply.github.com>
2023-11-28 12:59:41 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+def login():
+  while True:
+    username = input("What is your username?: ")
+    password = input("What is your password? ")
+    if username == "Meera" and password == "Code123#@":
+      print("Welcome Meera!")
+      break
+    else:
+      print("That is not the correct username or password. Try again!")
+      continue
+    
+print("REPLIT LOGIN SYSTEM")
+login()
+
+
+
+
+
+
+
+
+
+
+
+
+     
+ 
+ 
+ 
+  
+        
+
+  
+   
